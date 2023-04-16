f6f50c739d8c9fbe62d4c21d4716a3e6f0df701a
Wojciech Sikora <wsikora@vuestorefront.io>
2023-04-16 20:26:12 +0200

    feat: call createCart from app

--- app.vue
+++ app.vue
@@ -1,5 +1,38 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div id="checkout"></div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { embedCheckout } from "@bigcommerce/checkout-sdk";
+
+const cartRes = await fetch(
+  "http://localhost:3000/.netlify/functions/createCart"
+);
+const cart = await cartRes.json();
+
+console.log(cart)
+
+// const embeddedCheckoutUrl = cart.data.redirect_urls.embedded_checkout_url;
+
+// onMounted(async () => {
+//   await embedCheckout({
+//     containerId: "checkout",
+//     url: embeddedCheckoutUrl,
+//     onComplete: async () => {
+//       console.log("Checkout completed");
+//     },
+//     onError: async () => {
+//       console.log("Checkout failed.");
+//     },
+//     onFrameError: () => {
+//       console.log("Issue with checkout frame.");
+//     },
+//     onSignOut: async () => {
+//       console.log("User signed out.");
+//     },
+//   });
+// });
+</script>
