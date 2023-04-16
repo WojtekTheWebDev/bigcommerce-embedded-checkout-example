f00092672583389d638728b5002589776d3c6a00
Wojciech Sikora <wsikora@vuestorefront.io>
2023-04-16 22:46:25 +0200

    refactor: clean embedded checkout depts

--- app.vue
+++ app.vue
@@ -5,36 +5,16 @@
 </template>
 
 <script setup lang="ts">
-// import { onMounted } from "vue";
-// import { embedCheckout } from "@bigcommerce/checkout-sdk";
+import { onMounted } from "vue";
 
 const config = useRuntimeConfig();
 
-const cartRes = await fetch(
-  `${config.public.baseURL}/.netlify/functions/createCart`
-);
-const cart = await cartRes.json();
+onMounted(async () => {
+  const cartRes = await fetch(
+    `${config.public.baseURL}/.netlify/functions/createCart`
+  );
+  const cart = await cartRes.json();
 
-console.log(cart)
-
-// const embeddedCheckoutUrl = cart.data.redirect_urls.embedded_checkout_url;
-
-// onMounted(async () => {
-//   await embedCheckout({
-//     containerId: "checkout",
-//     url: embeddedCheckoutUrl,
-//     onComplete: async () => {
-//       console.log("Checkout completed");
-//     },
-//     onError: async () => {
-//       console.log("Checkout failed.");
-//     },
-//     onFrameError: () => {
-//       console.log("Issue with checkout frame.");
-//     },
-//     onSignOut: async () => {
-//       console.log("User signed out.");
-//     },
-//   });
-// });
+  console.log(cart);
+});
 </script>
